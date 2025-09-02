38214fe473dc68d2b30a22657f02b5be1953d61e
gerwyn <gerwynsim@hotmail.com>
2025-09-02 23:47:28 +0800

    setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-red-500">Welcome to nextjs</h1>;
+  return (
+    <>
+      <h1 className="text-red-500">Welcome to nextjs</h1>
+      <p className="font-space-grotesk text-blue-500">Welcome to nextjs</p>
+    </>
+  );
 }
