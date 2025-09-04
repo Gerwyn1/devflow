359ba0aed67d6271478a7159c73f558db414ddce
gerwyn <gerwynsim@hotmail.com>
2025-09-04 18:22:28 +0800

    home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,32 @@
-import { auth } from "@/auth";
-import LeftSidebar from "@/components/navigation/LeftSidebar";
-import Navbar from "@/components/navigation/navbar";
-import RightSidebar from "@/components/navigation/RightSidebar";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 export default async function Home({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const session = await auth();
-  console.log(session);
   return (
     <>
-      <h1>welcome to world of nextjs</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION} className="max-sm:w-full">
+            Ask a Question
+          </Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+        <p>Question Card 5</p>
+      </div>
     </>
   );
 }
