137898d428d3bd82f7e7c5e5314e8a0517bd6e8b
gerwyn <gerwynsim@hotmail.com>
2025-09-11 15:39:05 +0800

    Create Tags Action

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,13 @@
+import { getTags } from "@/lib/actions/tag.action";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({ page: 1, pageSize: 10, query:'react'  });
+
+  const { tags } = data || {};
+
+  console.log('TAGS', JSON.stringify(tags, null, 2));
+
+  return <div>Tags</div>;
+};
+
+export default Tags;
