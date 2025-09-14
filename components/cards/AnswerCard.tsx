f49ac1fae6980047e6a8c7d4c7a95e45c3471ea1
gerwyn <gerwynsim@hotmail.com>
2025-09-14 20:57:40 +0800

    Answer Votes

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -3,8 +3,13 @@ import { getTimeStamp } from "@/lib/utils";
 import Link from "next/link";
 import UserAvatar from "../UserAvatar";
 import { Preview } from "../editor/Preview";
+import Votes from "../votes/Votes";
+import { Suspense } from "react";
+import { hasVoted } from "@/lib/actions/vote.action";
 
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({ _id, author, content, createdAt, upvotes, downvotes }: Answer) => {
+  const hasVotedPromise = hasVoted({ targetId: _id, targetType: "answer" });
+
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span" />
@@ -28,16 +33,15 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
         </div>
 
         <div className="flex justify-end">
-          Votes
-          {/* <Suspense fallback={<div>Loading...</div>}> */}
-          {/* <Votes
+          <Suspense fallback={<div>Loading...</div>}>
+            <Votes
               targetType="answer"
               targetId={_id}
               hasVotedPromise={hasVotedPromise}
               upvotes={upvotes}
               downvotes={downvotes}
-            /> */}
-          {/* </Suspense> */}
+            />
+          </Suspense>
         </div>
       </div>
       <Preview content={content} />
