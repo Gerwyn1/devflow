2f8d17ec4d0ad6e769653dd0ef80041a667cdd07
gerwyn <gerwynsim@hotmail.com>
2025-09-15 23:29:50 +0800

    Show Popular Tags

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -273,3 +273,13 @@ export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
     return handleError(error) as ErrorResponse;
   }
 }
+
+export async function getTopTags(): Promise<ActionResponse<Tag[]>> {
+  try {
+    await dbConnect();
+    const tags = await Tag.find().sort({ questions: -1 }).limit(5);
+    return { success: true, data: JSON.parse(JSON.stringify(tags)), status: 200 };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
