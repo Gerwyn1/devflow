977a98ce3ad19b9a963b6efa372a871fe49c5a30
gerwyn <gerwynsim@hotmail.com>
2025-09-16 01:06:11 +0800

    Develop Profile Header

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,75 @@
-const Profile = () => {
+import { auth } from "@/auth";
+import ProfileLink from "@/components/user/ProfileLink";
+import UserAvatar from "@/components/UserAvatar";
+import { getUser } from "@/lib/actions/user.action";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import dayjs from "dayjs";
+import { Button } from "@/components/ui/button";
+
+const Profile = async ({ params }: RouteParams) => {
+  const { id } = await params;
+
+  if (!id) notFound();
+
+  const loggedInUser = await auth();
+
+  const { success, data, error } = await getUser({
+    userId: id,
+  });
+
+  if (!success)
+    return (
+      <div>
+        <div className="h1-bold text-dark100_light900">{error?.message}</div>
+      </div>
+    );
+
+  const { user, totalQuestions, totalAnswers } = data!;
+
   return (
-    <div>Profile</div>
-  )
-}
-export default Profile
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={user._id}
+            name={user.name}
+            imageUrl={user.image}
+            className="size-[140px] rounded-full object-cover"
+            fallbackClassName="text-6xl font-bolder"
+          />
+
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{user.name}</h2>
+            <p className="paragraph-regular text-dark200_light800">@{user.username}</p>
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {user.portfolio && <ProfileLink imgUrl="/icons/link.svg" href={user.portfolio} title="Portfolio" />}
+              {user.location && <ProfileLink imgUrl="/icons/location.svg" title={user.location} />}
+
+              <ProfileLink imgUrl="/icons/calendar.svg" title={dayjs(user.createdAt).format("MMMM YYYY")} />
+            </div>
+
+            {user?.bio && <p className="paragraph-regular text-dark400_light800 mt-8">{user.bio}</p>}
+          </div>
+        </div>
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href="/profile/edit">
+              <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
+          )}
+        </div>
+        {/* <Stats
+          totalQuestions={userStats?.totalQuestions || 0}
+          totalAnswers={userStats?.totalAnswers || 0}
+          badges={userStats?.badges || { GOLD: 0, SILVER: 0, BRONZE: 0 }}
+          reputationPoints={user.reputation || 0}
+        /> */}
+      </section>
+    </>
+  );
+};
+export default Profile;
